89dfa1041a74ca683697887a2cd8307efd787069
Aidan Loten <asloten@gmail.com>
2021-08-26 14:47:22 -0400

    server fix static folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
+const path = require('path');
 
 //Connect Database
 connectDB();
@@ -18,6 +19,16 @@ app.use(morgan('tiny'));
 app.use('/api/register', require('./routes/register'));
 app.use('/api/bottles', require('./routes/bottles.js'));
 app.use('/api/login', require('./routes/login.js'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
