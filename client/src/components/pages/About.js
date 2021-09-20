a61a7c4c2190e563d2b56e04c7c543c8cb967ba6
Aidan Loten <asloten@gmail.com>
2021-09-20 19:40:59 -0400

    About page update

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  item: {
+    display: 'inline',
+    fontSize: '40px',
+    marginRight: '20px',
+  },
+}));
 
 const About = () => {
+  const classes = useStyles();
   return (
     <div>
       <h1>About This App</h1>
@@ -8,7 +18,36 @@ const About = () => {
         This is a full-stack React app for wine cellar inventory management and
         social networking
       </p>
-      <p className='bg-dark p'>
+      <ul>
+        <li className={classes.item}>
+          <a href='https://github.com/aloten/terracotta-v1' target='_blank'>
+            <i class='fab fa-github-square'></i>
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            href='https://www.linkedin.com/in/aidan-loten-a009b9141/'
+            target='_blank'
+          >
+            <i class='fab fa-linkedin'></i>
+          </a>
+        </li>
+      </ul>
+      <ul class='nolink-icon'>
+        <li>
+          <a href='#'>
+            <i class='fas fa-envelope-square'></i>
+          </a>{' '}
+          asloten@gmail.com
+        </li>
+        <li id='phone-icon'>
+          <a href='#'>
+            <i class='fas fa-phone-square'></i>
+          </a>{' '}
+          +1 (914) 274-7643
+        </li>
+      </ul>
+      <p className='bg-dark p' style={{ marginTop: '10px' }}>
         <strong>Version: </strong> 1.0.0
       </p>
     </div>
